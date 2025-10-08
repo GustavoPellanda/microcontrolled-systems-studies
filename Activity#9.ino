0b85626725a7b1ae66a7b1be6a684bc3526262d5
GustavoPellanda <gusfipe@gmail.com>
2025-10-08 03:41:44 -0300

    State Handler class created.

--- Activity#9.ino
+++ Activity#9.ino
@@ -1,23 +1,19 @@
 #include <dht.h>
 
 /*
-DHTRead is a singleton class.
+The DHTRead class returns the temperature and humidity separately.
 It receives a pin to be read and a reference to a dht object.
 The shouldReadSensor() function ensures calling the getter methods will not trigger 
 the readSensor() function faster than the interval defined by readInterval.
-Returns the temperature and humidity separately.
 */
 class DHTRead {
 private:
-  static DHTRead* instance;
   const byte pin;
   dht &sensor;
   int temperature;
   int humidity;
   unsigned long lastReadTime;
   const unsigned long readInterval = 2000; // Reads every two seconds
-
-  DHTRead(byte p, dht& dhtRef) : pin(p), sensor(dhtRef), temperature(0), humidity(0), lastReadTime(0) {}
 
   void readSensor() {
     sensor.read11(pin);
@@ -31,27 +27,8 @@ private:
   }
 
 public:
-  DHTRead(const DHTRead&) = delete;
-  DHTRead& operator=(const DHTRead&) = delete;
-
-  static DHTRead* getInstance(byte p, dht& dhtRef) {
-    if (instance == nullptr) {
-      instance = new DHTRead(p, dhtRef);
-    }
-    return instance;
-  }
-
-  static DHTRead* getInstance() {
-    return instance;
-  }
-
-  static void deleteInstance() {
-    if (instance != nullptr) {
-      delete instance;
-      instance = nullptr;
-    }
-  }
-
+  DHTRead(byte p, dht& dhtRef) : pin(p), sensor(dhtRef), temperature(0), humidity(0), lastReadTime(0) {}
+  
   int getTemperature() {
     if (shouldReadSensor()) {
       readSensor();
@@ -66,9 +43,6 @@ public:
     return humidity;
   }
 };
-
-// Initializes static member
-DHTRead* DHTRead::instance = nullptr;
 
 /*
 Handles the serial prints:
@@ -86,6 +60,52 @@ public:
     Serial.println(" %");
     Serial.println("-------------------");
   }
+
+  void printState(byte state) {
+    Serial.print("State: ");
+    if(state == 1) Serial.println("green");
+    if(state == 2) Serial.println("yellow");
+    if(state == 3) Serial.println("red");
+  }
+};
+
+/*
+Handles the states of the system's operations 
+based on the temperature and humidity:
+*/
+class StateHandler {
+private:
+  SerialMonitorHandler &serialMonitor;
+
+public:
+  StateHandler(SerialMonitorHandler &serialRef) : serialMonitor(serialRef) {}
+
+  void analiseState(int temp, int humidity) {
+    if(temp >= 30) {
+      redAlert();
+      return;
+    }
+    if(temp >= 25 && humidity < 20) {
+      yellowOperation();
+      return;
+    }
+    if(temp < 30) {
+      greenOperation();
+      return;
+    }
+  }
+
+  void greenOperation() {
+    serialMonitor.printState(1);
+  }
+
+  void yellowOperation() {
+    serialMonitor.printState(2);
+  }
+
+  void redAlert() {
+    serialMonitor.printState(3);
+  }
 };
 
 /*
@@ -94,15 +114,16 @@ The read11() function from the library populates its attributes with float value
 of temperature in °C and humidity in relative percentage.
 */
 dht DHT;
+DHTRead dhtRead(8, DHT);
 SerialMonitorHandler serialMonitor;
+StateHandler stateHandler(serialMonitor);
 
 void setup() {
   Serial.begin(9600);
-  DHTRead::getInstance(8, DHT); // Initializes the singleton instance
 }
 
 void loop() {
   delay(2000);
-  DHTRead* dhtRead = DHTRead::getInstance(); // Ensures there is only one instance of the DHTRead class
-  serialMonitor.printSensorValues(dhtRead->getTemperature(), dhtRead->getHumidity());
+  stateHandler.analiseState(dhtRead.getTemperature(), dhtRead.getHumidity());
+  serialMonitor.printSensorValues(dhtRead.getTemperature(), dhtRead.getHumidity());
 }
